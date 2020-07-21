804d7e06e8c3a492d4fe271fc2f8936d7ec603db
David Heizer <DavidHeizer@QuickenLoans.com>
2020-07-21 11:33:01 -0400

    Added comments to code

--- Program.cs
+++ Program.cs
@@ -8,20 +8,20 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
-            string input = "Greetings";
+            string input = "sdgthn";
             input = input.ToLower();
             string pigLatin = "";
-
-            int firstvowel = CheckVowel(input);
+            int firstvowel = 0;
             //Console.WriteLine($"The first vowel is at position {firstvowel}");
             //string almostPigLatin = MoveConsonants(input, firstvowel);
             //Console.WriteLine($"The first step in converting a word to pig latin is: {almostPigLatin}");
 
-            if (firstvowel == 0)
+            firstvowel = CheckVowel(input);
+            if (firstvowel == 0) // Is there a vowel or is the firt letter a vowel if so first vowel will be 0
             {
                 pigLatin = AddWay(input);
             }
-            else
+            else // If the word starts with a consonant firstvowel will not be 0
             {
                 pigLatin = AddAy(MoveConsonants(input, firstvowel));
             }
@@ -45,14 +45,18 @@ namespace PigLatin
         {
             string changedWord = "";
             Queue<char> letters = new Queue<char>();
+
+            // Putting all the letters first vowel till the end into the queue letters including the first vowel
             for (int i=vowelPosition; i < word.Length; i++)
             {
                 letters.Enqueue(word[i]);
             }
+            // Putting all the letters before the first vowel into the queue letters
             for(int i = 0; i < vowelPosition; i++)
             {
                 letters.Enqueue(word[i]);
             }
+            // Pulling all the letters out of the queue to make the new string
             int queueSize = letters.Count;
             for (int i = 0; i < queueSize; i++)
             {
@@ -78,7 +82,7 @@ namespace PigLatin
                 }
             }
 
-            //Loop looking for first 'Y'
+            //Loop looking for first 'y' in case thier are no vowels
             for (int i = 0; i < length; i++)
             {
                 if (word[i] == 'y')
@@ -88,6 +92,10 @@ namespace PigLatin
                 }
             }
 
+            // Return logic 
+            // returning first vowel index
+            // returning first 'y' index if no vowels
+            // returning 0 if no vowels or y 
             if (vowelPosition == 0)
             {
                 return yPosition;
